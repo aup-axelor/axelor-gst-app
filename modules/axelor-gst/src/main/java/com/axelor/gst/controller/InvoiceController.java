2c6f0f143c9aadef76c799985cb562709773f73d
aup-axelor <a.updhyay@axelor.com>
2021-07-27 18:42:18 +0530

    sequence updated in invoice

--- modules/axelor-gst/src/main/java/com/axelor/gst/controller/InvoiceController.java
+++ modules/axelor-gst/src/main/java/com/axelor/gst/controller/InvoiceController.java
@@ -1,4 +1,20 @@
 package com.axelor.gst.controller;
 
+import com.axelor.gst.service.SequenceService;
+import com.axelor.rpc.ActionRequest;
+import com.axelor.rpc.ActionResponse;
+import com.google.inject.Inject;
+
 public class InvoiceController {
+	
+	  @Inject
+	  SequenceService sequenceService;
+	
+    public void getReference(ActionRequest request, ActionResponse response){
+
+      String reference= sequenceService.getReference("Invoice");
+      if(reference=="") response.setError("No sequence is specified for the party");
+      else response.setValue("reference",reference);
+
+  }
 }
