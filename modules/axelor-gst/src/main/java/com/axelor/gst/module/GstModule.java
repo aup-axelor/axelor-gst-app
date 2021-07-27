27c40ea4bed30eb55e2d906cecc96b3b953f1243
aup-axelor <a.updhyay@axelor.com>
2021-07-27 13:23:50 +0530

    rename of GST modeule

--- modules/axelor-gst/src/main/java/com/axelor/gst/module/GstModule.java
+++ modules/axelor-gst/src/main/java/com/axelor/gst/module/GstModule.java
@@ -4,7 +4,7 @@ import com.axelor.app.AxelorModule;
 import com.axelor.gst.service.SequenceService;
 import com.axelor.gst.service.impl.SequenceServiceImpl;
 
-public class GSTModule extends AxelorModule {
+public class GstModule extends AxelorModule {
     @Override
     protected void configure() {
         bind(SequenceService.class).to(SequenceServiceImpl.class);
