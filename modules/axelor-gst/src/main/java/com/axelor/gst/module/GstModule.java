36a5376b651a91caea04cf66c6acf609b72cfe2e
aup-axelor <a.updhyay@axelor.com>
2021-08-06 03:12:14 +0530

    added modules

--- modules/axelor-gst/src/main/java/com/axelor/gst/module/GstModule.java
+++ modules/axelor-gst/src/main/java/com/axelor/gst/module/GstModule.java
@@ -1,12 +1,19 @@
 package com.axelor.gst.module;
 
 import com.axelor.app.AxelorModule;
+import com.axelor.gst.service.InvoiceLineService;
+import com.axelor.gst.service.InvoiceService;
 import com.axelor.gst.service.SequenceService;
+import com.axelor.gst.service.impl.InvoiceLineServiceImpl;
+import com.axelor.gst.service.impl.InvoiceServiceImpl;
 import com.axelor.gst.service.impl.SequenceServiceImpl;
 
 public class GstModule extends AxelorModule {
     @Override
     protected void configure() {
         bind(SequenceService.class).to(SequenceServiceImpl.class);
+        bind(InvoiceLineService.class).to(InvoiceLineServiceImpl.class);
+        bind(InvoiceService.class).to(InvoiceServiceImpl.class);
+        
     }
 }
